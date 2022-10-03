0c8147d6fb1e927531a69f02dc9d0c542711bad8
Sam Glass <sam.charles.glass@gmail.com>
2022-10-03 10:55:54 +0200

    test vectors

---
+++ code alongs/Lec14-unit-test/tests/test_vector.py
@@ -0,0 +1,20 @@
+import sys, os
+
+#print(__file__)
+
+# we change directory to where this file is
+os.chdir(os.path.dirname(__file__))
+
+# we define a path that is one step
+# in this path we have a vector.py and plotter.py and manual_testing.ipynb
+path_to_vector_module = os.path.abspath("../")
+
+sys.path.append(path_to_vector_module)
+print(path_to_vector_module)
+
+from vector import Vector
+
+
+
+
+ #from vector import Vector
